ecc23ce68defe65cfb5e7bbe3b492c64bdb1be56
abhishek271006-commits <abhishek271006@gmail.com>
2026-03-13 09:13:45 +0530

    new code

--- C++/pattern.cpp
+++ C++/pattern.cpp
@@ -100,6 +100,11 @@
 //     }
 //     return 0;
 //}
+//question to print a triangle pattern 
+// 1
+// 22
+// 333
+// 4444
 // #include<iostream>
 // using namespace std;
 // int main(){
@@ -111,6 +116,24 @@
 //         cout<<endl;
 //     }
 // return 0;
-// }
+ // }
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int n=4;
+    char ch='A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+        
+        cout<<ch;
+
+
+        }
+        cout<<endl;
+          ch++;
+
+    }
+    return 0;
+ }
 
     
